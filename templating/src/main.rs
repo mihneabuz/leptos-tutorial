aa8bc5eb074375f2350c6227631b3937d93d1b0b
mihneabuz <mihneabuzatu88@gmail.com>
2024-02-26 20:56:07 +0200

    also trying out maud templating

--- templating/src/main.rs
+++ templating/src/main.rs
@@ -1,4 +1,5 @@
 use leptos::view;
+use maud::html;
 
 use axum::{
     response::{Html, IntoResponse},
@@ -12,7 +13,9 @@ async fn main() {
         .route("/", get(root))
         .route("/home", get(home))
         .route("/leptos", get(leptos))
-        .route("/leptos_explicit", get(leptos_explicit));
+        .route("/leptos_explicit", get(leptos_explicit))
+        .route("/maud", get(maud))
+        .route("/maud_explicit", get(maud_explicit));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap()
@@ -49,7 +52,34 @@ async fn leptos() -> impl IntoResponse {
     leptos_html! (
       <div>
         <h2>"Hello Leptos!"</h2>
-        <h4>"view macro rocks!"</h4>
+        <h4>"wrapped view macro rocks!"</h4>
       </div>
     )
+}
+
+async fn maud_explicit() -> impl IntoResponse {
+    Html::from(
+        html! {
+            div {
+                h2 { "Hello Maud!" }
+                h4 { "macros are za best" }
+            }
+        }
+        .into_string(),
+    )
+}
+
+macro_rules! maud_html {
+    ($($x:tt)*) => {
+        Html::from(html! { $($x)* }.into_string())
+    };
+}
+
+async fn maud() -> impl IntoResponse {
+    maud_html! {
+        div {
+            h2 { "Hello Maud!" }
+            h4 { "macros are za best" }
+        }
+    }
 }
